d729071f5dc38daf15c99dcb27cee27817bb97ac
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-11-13 14:12:40 +0100

    Update PERT.h

--- PERT-method/src/include/PERT.h
+++ PERT-method/src/include/PERT.h
@@ -6,34 +6,41 @@
 using namespace std;
 
 class PERT {
-    public:
+public:
     int N;  // task number
     int M;  // connection number
     float processTime;
-    float CRPwar; 
-    float CRPtime; 
+    float CRPwar;
+    float CRPtime;
+    int XX;
+    double Y;
 
     vector <vector <int>> X;            // adjacency matrix
     vector <vector <float>> TaskTimes;    // times for particular tasks
     vector <vector <float>> CritPath;     // critical path: task_num, start_t, finish_t
-    vector <vector <int>> inputTripleTime;     
+    vector <vector <int>> inputTripleTime;
     vector <float> war;
-    private:
-        bool cycle;
-        vector <float> P;
-        vector <float> C;
+private:
+    bool cycle;
+    vector <int> taskDuration;
+    vector <int> earlyStart;
+    vector <int> earlyFinish;
+    vector <int> lateStart;
+    vector <int> lateFinish;
 
-    public:
-        PERT();
-        ~PERT();
-        void importData(string fileName);
-        void printAdjacencyMatrix();
-        void printTaskTimes();
-        void findProcessTime();
-        void findTaskTimes();
-        void findCRP();
-        float finishPossibilityIn(float duration);
-        float projectLenthWithPossibility(float possibility);
-        
+public:
+    PERT();
+    ~PERT();
+    void importData(string fileName);
+    void printAdjacencyMatrix();
+    void printTaskTimes();
+    void findProcessTime();
+    void findTaskTimes();
+    void findCRP();
+    void dys();
+    double inv_dys(double);
+    void time_day();
+    
+
 };
 
