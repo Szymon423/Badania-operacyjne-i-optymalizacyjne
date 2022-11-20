7fb14944329a13ff24a14fac8e7ac7975f3101f7
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-11-20 21:30:13 +0100

    Create BelmanFord.cpp

---
+++ Shortest-path/source/BelmanFord.cpp
@@ -0,0 +1,61 @@
+#include "BellmanFord.h"
+#include <fstream>
+
+BellmanFord::BellmanFord() {
+	BellmanFord::N = 0;
+}
+
+BellmanFord::~BellmanFord() {
+
+}
+
+void BellmanFord::importData(string path) {
+	ifstream f(path);
+
+	// odczytanie wartości N
+	f >> BellmanFord::N;
+	int help{ 0 };
+	vector <int> helpV(BellmanFord::N, 0);
+
+	for (int a = 0; a < BellmanFord::N; a++) {
+		// stworzenie wektora pomocniczego do uzupełnienia macierzy X
+		for (int b = 0; b < BellmanFord::N; b++) {
+			f >> help;
+			helpV.at(b) = help;
+		}
+		BellmanFord::X.push_back(helpV);
+
+		// utworzenie wektora dist o odpowiedniej długości
+		// tam gdzie jest zero, oznacza, że nie ma krawędzi
+		// dlatego wektor uzupełniamy względnie dużą liczbą
+		if (a == 0) {
+			BellmanFord::dist.push_back(0);
+		}
+		else {
+			BellmanFord::dist.push_back(99999);
+		}
+	}
+	f.close();
+}
+
+
+void BellmanFord::doAllMagic() {
+	for (int i = 0; i < BellmanFord::N; i++) {
+		for (int a = 0; a < BellmanFord::N; a++) {
+			for (int b = 0; b < BellmanFord::N; b++) {
+				if (BellmanFord::X.at(a).at(b) > 0) {
+					BellmanFord::dist[b] = min(BellmanFord::dist[b], BellmanFord::dist[a] + X.at(a).at(b));
+				}
+			}
+		}
+	}
+}
+
+
+void BellmanFord::show() {
+	cout << "Nr wezla \tDystans" << endl;
+	for (int a = 0; a < BellmanFord::N; a++) {
+		cout << a + 1 << " \t\t" << BellmanFord::dist.at(a) << endl;
+	}
+}
+
