e2caf2d2dbd340ee71bda761deb5cd72e52c92d6
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-11-21 20:11:43 +0100

    Update Djikstra.cpp

--- Shortest-path/source/Djikstra.cpp
+++ Shortest-path/source/Djikstra.cpp
@@ -3,57 +3,59 @@
 
 #define bigNum 99999
 
-Djikstra::Djikstra() {
-	Djikstra::N = 0;
-	Djikstra::src = 0;
-	Djikstra::cycle = false;
+Dijkstra::Dijkstra() {
+	Dijkstra::N = 0;
+	Dijkstra::src = 0;
+	Dijkstra::cycle = false;
+	Dijkstra::cnt = 0;
 }
 
-Djikstra::~Djikstra() {
+Dijkstra::~Dijkstra() {
 
 }
 
-void Djikstra::importData(string path) {
+void Dijkstra::importData(string path) {
 	ifstream f(path);
 
 	// odczytanie wartości N
-	f >> Djikstra::N;
+	f >> Dijkstra::N;
 	int help{ 0 };
-	vector <int> helpV(Djikstra::N, 0);
-	Djikstra::cycle = false;
+	vector <int> helpV(Dijkstra::N, 0);
+	Dijkstra::cycle = false;
 
-	for (int a = 0; a < Djikstra::N; a++) {
+	for (int a = 0; a < Dijkstra::N; a++) {
 		// stworzenie wektora pomocniczego do uzupełnienia macierzy X
-		for (int b = 0; b < Djikstra::N; b++) {
+		for (int b = 0; b < Dijkstra::N; b++) {
 			f >> help;
 			helpV.at(b) = help;
 		}
-		Djikstra::X.push_back(helpV);
+		Dijkstra::X.push_back(helpV);
 
 		// utworzenie wektora dist o odpowiedniej długości
 		// tam gdzie jest zero, oznacza, że nie ma krawędzi
 		// dlatego wektor uzupełniamy względnie dużą liczbą
-		Djikstra::dist.push_back(bigNum);
-		Djikstra::isOk.push_back(false);
-		Djikstra::path.push_back(-1);
+		Dijkstra::dist.push_back(bigNum);
+		Dijkstra::isOk.push_back(false);
+		Dijkstra::path.push_back(-1);
 	}
 	f.close();
 	return;
 }
 
 
-void Djikstra::doAllMagic() {
+void Dijkstra::doAllMagic() {
 	//cout << endl;
-	for (int a = 0; a < Djikstra::N; a++) {
+	for (int a = 0; a < Dijkstra::N; a++) {
 		// znalezienie sąsiada z minimalną odległością
-		int nearesNeighbour = Djikstra::minNeighbour();
-		Djikstra::isOk.at(nearesNeighbour) = true;
+		int nearesNeighbour = Dijkstra::minNeighbour();
+		Dijkstra::isOk.at(nearesNeighbour) = true;
 			
-		for (int b = 0; b < Djikstra::N; b++) {
-			if (Djikstra::X.at(nearesNeighbour).at(b) > 0 && !Djikstra::isOk.at(b) && Djikstra::dist.at(nearesNeighbour) != bigNum) {
-				if (Djikstra::dist.at(nearesNeighbour) + X.at(nearesNeighbour).at(b) < Djikstra::dist.at(b)){
-					Djikstra::dist.at(b) = Djikstra::dist.at(nearesNeighbour) + X.at(nearesNeighbour).at(b);
-					Djikstra::path.at(b) = nearesNeighbour;
+		for (int b = 0; b < Dijkstra::N; b++) {
+			if (Dijkstra::X.at(nearesNeighbour).at(b) > 0 && !Dijkstra::isOk.at(b) && Dijkstra::dist.at(nearesNeighbour) != bigNum) {
+				if (Dijkstra::dist.at(nearesNeighbour) + X.at(nearesNeighbour).at(b) < Dijkstra::dist.at(b)){
+					Dijkstra::dist.at(b) = Dijkstra::dist.at(nearesNeighbour) + X.at(nearesNeighbour).at(b);
+					Dijkstra::path.at(b) = nearesNeighbour;
+					Dijkstra::cnt++;
 				}	
 			}
 		}
@@ -62,34 +64,35 @@ void Djikstra::doAllMagic() {
 }
 
 
-void Djikstra::show() {
-	cout << "Najkrotsze sciezki z wierzchołka " << Djikstra::src << endl;
+void Dijkstra::show() {
+	cout << "Najkrotsze sciezki z wierzchołka " << Dijkstra::src << endl;
 	cout << "dlugosc trasy\t\t Najkrotsza sciezka" << endl;
 	
-	for (int i = 0; i < Djikstra::N; i++) {
-		if (i != Djikstra::src) {
-			if (Djikstra::dist.at(i) == bigNum) Djikstra::dist.at(i) = -1;
-			cout << Djikstra::dist.at(i) << " \t\t";
-			if (Djikstra::dist.at(i) != -1)	Djikstra::printPath(i, Djikstra::path);
+	for (int i = 0; i < Dijkstra::N; i++) {
+		if (i != Dijkstra::src) {
+			if (Dijkstra::dist.at(i) == bigNum) Dijkstra::dist.at(i) = -1;
+			cout << Dijkstra::dist.at(i) << " \t\t";
+			if (Dijkstra::dist.at(i) != -1)	Dijkstra::printPath(i, Dijkstra::path);
 		}
 		cout << endl;
 	}
+	cout << endl << "Liczba operacji dla Djikstry: " << Dijkstra::cnt << endl;
 	return;
 }
 
-void Djikstra::printPath(int currentVertex, vector <int> path_) {
+void Dijkstra::printPath(int currentVertex, vector <int> path_) {
 	if (currentVertex == -1) return;
 	printPath(path_[currentVertex], path_);
 	cout << " " << currentVertex << " ";
 }
 
 
-int Djikstra::minNeighbour() {
+int Dijkstra::minNeighbour() {
 	int minVal{ bigNum };
 	int minIndex{ 0 };
-	for (int i = 0; i < Djikstra::N; i++) {
-		if (Djikstra::dist.at(i) < minVal && !Djikstra::isOk.at(i)) {
-			minVal = Djikstra::dist.at(i);
+	for (int i = 0; i < Dijkstra::N; i++) {
+		if (Dijkstra::dist.at(i) < minVal && !Dijkstra::isOk.at(i)) {
+			minVal = Dijkstra::dist.at(i);
 			minIndex = i;
 		}
 	}
@@ -97,8 +100,8 @@ int Djikstra::minNeighbour() {
 }
 
 
-void Djikstra::source(int _src) {
-	Djikstra::src = _src;
-	Djikstra::dist.at(_src) = 0;
-	Djikstra::isOk.at(_src) = false;
+void Dijkstra::source(int _src) {
+	Dijkstra::src = _src;
+	Dijkstra::dist.at(_src) = 0;
+	Dijkstra::isOk.at(_src) = false;
 }
