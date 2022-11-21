93d305ba69b24b5735720060b1f84208626e2eb6
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2022-11-21 20:12:17 +0100

    Update main.cpp

--- Shortest-path/source/main.cpp
+++ Shortest-path/source/main.cpp
@@ -5,37 +5,30 @@
 
 
 int main() {
-	
-	// pomiar czasu dla Bellmana Forda:
-	auto startBF = chrono::high_resolution_clock::now();
-
 	// realizacja algorytmu
 	BellmanFord bf;
 	bf.importData("data.txt");
+	auto startBF = chrono::high_resolution_clock::now();
 	bf.doAllMagic();
 	auto stopBF = chrono::high_resolution_clock::now();
 	bf.show();
 
 	//wyświetlanie czasu
-	
 	auto durationBF = chrono::duration_cast<chrono::microseconds>(stopBF - startBF);
-	cout << endl << "Czas trwania obliczen dla Bellmana Forda: " << durationBF.count() << " mikrosekund" << endl;
-
-	// pomiar czasu dla Djikstry:
-	auto startDJ = chrono::high_resolution_clock::now();
+	cout << endl << "Czas trwania obliczen dla Bellmana Forda: " << durationBF.count() << " mikrosekund" << endl;	
 
 	// realizacja algorytmu
-	Djikstra dj;
+	Dijkstra dj;
 	dj.importData("data.txt");
 	dj.source(0);
+	auto startDJ = chrono::high_resolution_clock::now();
 	dj.doAllMagic();
 	auto stopDJ = chrono::high_resolution_clock::now();
 	dj.show();
 
 	//wyświetlanie czasu
-	
 	auto durationDJ = chrono::duration_cast<chrono::microseconds>(stopDJ - startDJ);
-	cout << endl << "Czas trwania obliczen dla Bellmana Forda: " << durationDJ.count() << " mikrosekund" << endl;
+	cout << endl << "Czas trwania obliczen dla Dijkstry: " << durationDJ.count() << " mikrosekund" << endl;
 
 	return 0;
 }
