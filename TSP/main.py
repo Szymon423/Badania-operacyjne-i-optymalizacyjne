93c76d753856ef0fc4e18934008c7d5d368b188d
Szymon Wąchała <96399051+Szymon423@users.noreply.github.com>
2023-01-04 15:24:20 +0100

    Update main.py

--- TSP/main.py
+++ TSP/main.py
@@ -2,12 +2,37 @@ import time
 import numpy as np
 import tsp 
 
-
+# okreslenie ścieżki do pliku testowego
 file = "test\\data_51.txt"
 
+# inicjalizacja obiektu 
 test = tsp.TSP()
 
+# importowanie danych z pliku
 test.importData(file)
+
+# określenie miasta początkowego
 test.createFirstTour(3)
+
+# określenie max liczby interacji dla SA
+maxIterationNumber = 50_000
+
+# określenie max temperatury dla SA
+maxTemperature = 10
+
+# odaplamy timer
+start = time.time_ns()
+
+# nasze dwa algorytmy do testów
+# test.SA(maxIterationNumber, maxTemperature)
 test.opt2()
+
+# znowu odpalamy timer
+finish = time.time_ns()
+
+# liczymy czas egzekucji
+elapsedTime = finish - start
+print("czas trwania: " + str(elapsedTime // 1_000_000) + "ms")
+
+# printowanie rozwiązania
 test.showSolution()
